01bcc89e66b99949d8b10925385102d78f2c5c95
박경은 <ruddms936@naver.com>
2021-06-10 12:30:17 +0900

    add: sha265 암호화 예제 코드 (git config 변경)

---
+++ EncryptionHelper.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace helloworld_c_sharp
+{
+  public class EncryptionHelper
+  {
+    public static string EncryptionSHA256(string password)
+    {
+      // sha256
+      var sha256Managed = new SHA256Managed();
+      byte[] encryptBytes = sha256Managed.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+      // base64
+      string encryptString = Convert.ToBase64String(encryptBytes);
+
+      return encryptString;
+    }
+  }
+}

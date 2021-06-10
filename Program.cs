c8d9b474f01115ad109cdd657a8bd9b256384f22
박경은 <kyungeun@sds.co.kr>
2021-06-10 12:26:56 +0900

    add: sha265 암호화 예제 코드

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace helloworld_c_sharp
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      // sha256 to base64 sample
+      Console.Write("패스워드를 입력해주세요: ");
+      string password = Console.ReadLine();
+      string encrypt = EncryptionHelper.EncryptionSHA256(password);
+      Console.WriteLine($"base64: {encrypt}");
     }
   }
 }

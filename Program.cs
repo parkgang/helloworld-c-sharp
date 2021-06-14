f1a633f9723caa8328f6ab92b487aeb95ac12eb6
박경은 <ruddms936@naver.com>
2021-06-14 10:57:22 +0900

    add: 유저 아이디와 이메일 주소를 분리하는 예제 코드 추가

--- Program.cs
+++ Program.cs
@@ -6,6 +6,12 @@ namespace helloworld_c_sharp
   {
     static void Main(string[] args)
     {
+      // 이메일 주소에서 아이디와 도메인 분리
+      string email = "user01@test.co.kr";
+      int emailIndex = email.IndexOf("@");
+      string emailId = email.Substring(0, emailIndex);
+      Console.WriteLine(emailId);
+
       // sha256 to base64 sample
       Console.Write("패스워드를 입력해주세요: ");
       string password = Console.ReadLine();
